102e8a81068935e9ec0d5fe552e9b11df909377b
zhouzihan <zhouzihanntu@outlook.com>
2017-01-18 17:10:51 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
+# blog
+
+> A Vue.js project
+
 # dev-blog
 record issues I came across through development
+
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/Hello.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <button type="button" name="button" v-on:click="test"></button>
+    </div>
+</template>
+
+<script>
+// export default {
+//   name: 'hello',
+//   data () {
+//     return {
+//       msg: 'Welcome to Your Vue.js App'
+//     }
+//   }
+// }
+
+export default {
+    name: 'hello',
+    data () {
+        return {
+            msg: "hhh"
+        }
+    },
+    methods: {
+        test: function() {
+            alert(12);
+            axios.get('/user', {
+                    params: {
+                        ID: 12345
+                    }
+                })
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
