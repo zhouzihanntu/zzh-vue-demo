fd2884b06ca2168eb6e8314fd3931613313cbd83
zhouzihanntu <abbey357423606@hotmail.com>
2017-01-18 19:03:14 +0800

    init

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,33 +1,37 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <!-- <button type="button" name="button" v-on:click="test"></button> -->
+    </div>
 </template>
 
 <script>
+import axios from "../assets/js/axios.js"
+let baseUrl = "https://api.github.com/";
+
 export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    name: 'hello',
+    data () {
+        return {
+        msg: 'hhh'
+        }
+    },
+    methods: {
+        test: function() {
+            alert(22);
+            axios.get('/user', {
+                params: {
+                  ID: 12345
+                }
+              })
+              .then(function (response) {
+                console.log(response);
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+        }
     }
-  }
 }
 </script>
 
